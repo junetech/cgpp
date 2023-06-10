8efdcdb6091b156f4e4f059df21acca3839921b5
JuneTech <junetech0@gmail.com>
2023-06-10 21:39:57 +0900

    output xlsx change & MILP fix

--- cgpp/output_func.py
+++ cgpp/output_func.py
@@ -63,19 +63,27 @@ def make_santini_21_sch_xlsx(
     else:
         t_list = p_ins.shelf_type_list  # t\in T in paper
     k_list = p_ins.config_id_list  # k\in K in paper
-    full_d_list = p_ins.t_idx_list  # d\in D in paper; set of demand days
-    d_list = full_d_list[:-1]  # d\in D in paper; set of demand days
-    d_prime_list = [0] + d_list  # extended time horizon
+    demand_d_list = p_ins.t_idx_list  # d\in D in paper; set of demand days
+    growth_d_list = p_ins.t_idx_list[:-1]  # d\in D in paper; set of demand days
+    d_prime_list = [0] + growth_d_list  # extended time horizon
 
     # Parameters
     # c -> d -> demand quantity
     p_dict = p_ins.make_demand_dict()
     # c -> t -> the crop can grow on the shelf type
-    delta_dict = p_ins.crop_shelf_type_compatible
+    delta_dict: dict[str, dict[int, bool]]
+    if type(p_ins) == ProbInsS21T1:
+        delta_dict = p_ins.crop_shelf_compatible
+    else:
+        delta_dict = p_ins.crop_shelf_type_compatible
     # c -> list of compatible t
     T_dict = {c: [t for t in t_list if delta_dict[c][t]] for c in c_list}
     # t -> number of shelves
-    n_dict = p_ins.num_shelves
+    n_dict: dict[str, int]
+    if type(p_ins) == ProbInsS21T1:
+        n_dict = {t: 1 for t in t_list}
+    else:
+        n_dict = p_ins.num_shelves
     # c -> the number of growth days
     gamma_dict = p_ins.crop_growth_days
     # c -> g (growth day) -> configuration required
@@ -104,14 +112,19 @@ def make_santini_21_sch_xlsx(
     # schedule worksheet
     sch_ws: Worksheet = wb.worksheets[0]
     sch_ws.title = output_meta.schedule_sheetname
-
     # shelf configuration worksheet
     cfg_ws: Worksheet = wb.create_sheet(output_meta.shelf_config_sheetname)
+    # germination worksheet
+    grm_ws: Worksheet = wb.create_sheet(output_meta.germination_sheetname)
+    # harvest worksheet
+    hv_ws: Worksheet = wb.create_sheet(output_meta.harvest_sheetname)
 
-    ws_list: list[Worksheet] = [sch_ws, cfg_ws]
+    crop_ws_list: list[Worksheet] = [sch_ws, grm_ws, hv_ws]
+    shelf_ws_list: list[Worksheet] = [cfg_ws]
+    ws_list: list[Worksheet] = crop_ws_list + shelf_ws_list
 
-    cursor["row"] += 1
-    time_row = [output_meta.keystone_val] + p_ins.t_idx_list
+    cursor["row"] = 2
+    time_row = [output_meta.keystone_val] + [0] + p_ins.t_idx_list
 
     for ws in ws_list:
         ws.cell(**cursor).value = p_ins.problem_name
@@ -127,44 +140,100 @@ def make_santini_21_sch_xlsx(
             get_column_letter(cursor["column"])
         ].width = output_meta.first_col_wth
 
+    # Shelf configuration worksheet
     for shelf_id in t_list:
         cursor["row"] += 1
-        for ws in ws_list:
+        cfg_ws.cell(**cursor).value = shelf_id
+        for t_idx in d_prime_list:
+            for cfg_id in k_list:
+                if solution.y[shelf_id][t_idx][cfg_id] == 0:
+                    continue
+                col = (
+                    cursor["column"] + 1 + t_idx
+                )  # +1 since column t_idx starts from 0
+                cfg_ws.cell(column=col, row=cursor["row"]).value = cfg_id
+
+    # Crop information worksheets
+    cursor["row"] = 2
+    for shelf_id in t_list:
+        cursor["row"] += 1
+        for ws in crop_ws_list:
             ws.cell(**cursor).value = shelf_id
 
         row_key_list: list[str] = list()
-        sch_row_list_list: list[list[int]] = list()
+        grm_row_list_dict: dict[str, list[int]] = dict()  # row_key -> row_list
+        sch_row_list_dict: dict[str, list[int]] = dict()  # row_key -> row_list
+        hv_row_list_dict: dict[str, list[int]] = dict()  # row_key -> row_list
 
         for crop_id in c_list:
             if shelf_id not in T_prime_dict[crop_id]:
                 continue
-            for t_idx in d_prime_list:
-                sch_row_list_dict: dict[str, list[int]] = dict()  # [0] * p_ins.n_days
-                for g in range(gamma_dict[crop_id] + 1):
-                    for t1 in T_prime_dict[crop_id]:
-                        if solution.x[crop_id][g][t1][t_idx][shelf_id] == 0:
-                            continue
-                        tp = t_idx + 1 - g
-                        row_key = f"{crop_id}_{tp}"
-                        if row_key not in sch_row_list_dict:
-                            sch_row_list_dict[row_key] = [0] * p_ins.n_days
-                        sch_row_list_dict[row_key][t_idx] = solution.x[crop_id][g][t1][
-                            t_idx
-                        ][shelf_id]
-                for row_key, sch_row_list in sch_row_list_dict.items():
+            for t_idx in growth_d_list:
+                row_key = f"{crop_id}_{t_idx}"
+                if row_key not in row_key_list:
                     row_key_list.append(row_key)
-                    sch_row_list_list.append(sch_row_list)
+                grm_row_list_dict[row_key] = [0] * p_ins.n_days
+                grm_unit_count = solution.x[crop_id][0][sigma][t_idx][shelf_id]
+                if grm_unit_count > 0:
+                    grm_row_list_dict[row_key][t_idx] = grm_unit_count
 
-        for r_idx, row_key in enumerate(row_key_list):
+                for g in range(gamma_dict[crop_id] + 1):
+                    crop_unit_count = sum(
+                        solution.x[crop_id][g][shelf_id][t_idx][t2]
+                        for t2 in T_prime_dict[crop_id]
+                    )
+                    if crop_unit_count == 0:
+                        continue
+                    tp = t_idx - g
+                    row_key = f"{crop_id}_{tp}"
+                    if row_key not in row_key_list:
+                        row_key_list.append(row_key)
+                    if row_key not in sch_row_list_dict:
+                        sch_row_list_dict[row_key] = [0] * p_ins.n_days
+                    sch_row_list_dict[row_key][t_idx] = crop_unit_count
+
+                row_key = f"{crop_id}_{t_idx - gamma_dict[c]}"
+                if row_key not in row_key_list:
+                    row_key_list.append(row_key)
+                hv_row_list_dict[row_key] = [0] * p_ins.n_days
+                hv_unit_count = solution.x[crop_id][gamma_dict[c]][shelf_id][t_idx][tau]
+                if hv_unit_count > 0:
+                    hv_row_list_dict[row_key][t_idx + 1] = hv_unit_count
+
+        for row_key in row_key_list:
             cursor["row"] += 1
-            for ws in ws_list:
+            for ws in crop_ws_list:
                 ws.cell(**cursor).value = row_key
-            sch_row_list = sch_row_list_list[r_idx]
+            if row_key in sch_row_list_dict:
+                sch_row_list = sch_row_list_dict[row_key]
+            else:
+                sch_row_list = [0] * p_ins.n_days
+            if row_key in grm_row_list_dict:
+                grm_row_list = grm_row_list_dict[row_key]
+            else:
+                grm_row_list = [0] * p_ins.n_days
+            if row_key in hv_row_list_dict:
+                hv_row_list = hv_row_list_dict[row_key]
+            else:
+                hv_row_list = [0] * p_ins.n_days
+
+            if sum(sch_row_list) + sum(grm_row_list) + sum(hv_row_list) == 0:
+                break
+
             for c_idx, sch_row_val in enumerate(sch_row_list):
                 if sch_row_val == 0:
                     continue
                 col = cursor["column"] + 1 + c_idx
                 sch_ws.cell(column=col, row=cursor["row"]).value = sch_row_val
+            for c_idx, grm_row_val in enumerate(grm_row_list):
+                if grm_row_val == 0:
+                    continue
+                col = cursor["column"] + 1 + c_idx
+                grm_ws.cell(column=col, row=cursor["row"]).value = grm_row_val
+            for c_idx, hv_row_val in enumerate(hv_row_list):
+                if hv_row_val == 0:
+                    continue
+                hv_ws.cell(column=col, row=cursor["row"]).value = hv_row_val
 
     f_loc = output_meta.crop_schedule_f_loc(p_ins.problem_name, 3)
     wb.save(filename=f_loc)
--- cgpp/santini_21_milp_t3.py
+++ cgpp/santini_21_milp_t3.py
@@ -16,13 +16,23 @@ def t_list_after(t: int, t_list: list[int]) -> list[int]:
 
 
 def solve_santini_21_milp_t3(p_ins: ProbInsS21T3, solver_name: str, timelimit: int):
+    # from pprint import pprint
+
     # Indices
-    c_list = p_ins.crop_id_list  # c\in C in paper
-    t_list = p_ins.shelf_type_list  # t\in T in paper
-    k_list = p_ins.config_id_list  # k\in K in paper
-    full_d_list = p_ins.t_idx_list  # d\in D in paper; set of demand days
-    d_list = full_d_list[:-1]  # d\in D in paper; set of demand days
-    d_prime_list = [0] + d_list  # extended time horizon
+    C_list = p_ins.crop_id_list  # c\in C in paper
+    T_list = p_ins.shelf_type_list  # t\in T in paper
+    K_list = p_ins.config_id_list  # k\in K in paper
+    demand_D_list = p_ins.t_idx_list  # set of demand days
+    growth_D_list = p_ins.t_idx_list[:-1]  # d\in D in paper; set of growth days
+    D_prime_list = [0] + growth_D_list  # extended time horizon
+    # day 0 is the earliest day seeds can be planted, day 1 is the earliest day of growth counted
+
+    # print(C_list)
+    # print(T_list)
+    # print(K_list)
+    # print(demand_D_list)
+    # print(growth_D_list)
+    # print(D_prime_list)
 
     # Parameters
     # c -> d -> demand quantity
@@ -30,7 +40,7 @@ def solve_santini_21_milp_t3(p_ins: ProbInsS21T3, solver_name: str, timelimit: i
     # c -> t -> the crop can grow on the shelf type
     delta_dict = p_ins.crop_shelf_type_compatible
     # c -> list of compatible t
-    T_dict = {c: [t for t in t_list if delta_dict[c][t]] for c in c_list}
+    T_dict = {c: [t for t in T_list if delta_dict[c][t]] for c in C_list}
     # t -> number of shelves
     n_dict = p_ins.num_shelves
     # c -> the number of growth days
@@ -45,10 +55,20 @@ def solve_santini_21_milp_t3(p_ins: ProbInsS21T3, solver_name: str, timelimit: i
     # produce stage index
     tau = "ProduceStage"
     t_prime_list = [sigma] + p_ins.shelf_type_list + [tau]
-    for c in c_list:
+    for c in C_list:
         delta_dict[c][sigma] = True
         delta_dict[c][tau] = True
-    T_prime_dict = {c: [sigma] + T_dict[c] + [tau] for c in c_list}
+    T_prime_dict = {c: [sigma] + T_dict[c] + [tau] for c in C_list}
+
+    # pprint(p_dict)
+    # pprint(delta_dict)
+    # pprint(T_dict)
+    # pprint(n_dict)
+    # pprint(gamma_dict)
+    # pprint(k_dict)
+    # pprint(q_dict)
+    # pprint(t_prime_list)
+    # pprint(T_prime_dict)
 
     solver: Solver = Solver.CreateSolver(solver_name)
     infty = Solver.infinity()
@@ -66,47 +86,50 @@ def solve_santini_21_milp_t3(p_ins: ProbInsS21T3, solver_name: str, timelimit: i
                         )
                         for t2 in T_prime_dict[c]
                     }
-                    for d in d_prime_list
+                    for d in D_prime_list
                 }
                 for t1 in T_prime_dict[c]
             }
-            for g in range(gamma_dict[c] + 1)
+            for g in t_list_before(gamma_dict[c], D_prime_list)
         }
-        for c in c_list
+        for c in C_list
     }
     # the number of shelves of type t with configuration k on day d
     # TODO: shelf type에서 쓰이는 configuration list의 dictionary를 쓰자
     y = {
         t: {
-            d: {k: solver.IntVar(lb=0, ub=infty, name=f"y_{t},{d},{k}") for k in k_list}
-            for d in d_list
+            d: {k: solver.IntVar(lb=0, ub=infty, name=f"y_{t},{d},{k}") for k in K_list}
+            for d in D_prime_list
         }
-        for t in t_list
+        for t in T_list
     }
 
     # Objective 3: minimize unmet demand
     omega_dict = p_ins.missed_demand_penalty
+    # pprint(omega_dict)
     # c -> d -> the amount of unmet demand of crop c on day d
     u = {
-        c: {d: solver.IntVar(lb=0, ub=infty, name=f"u_{c},{d}") for d in full_d_list}
-        for c in c_list
+        c: {d: solver.IntVar(lb=0, ub=infty, name=f"u_{c},{d}") for d in demand_D_list}
+        for c in C_list
     }
     obj_func = solver.Objective()
-    for c, d in product(c_list, full_d_list):
+    for c, d in product(C_list, demand_D_list):
         if d not in omega_dict[c]:
             continue
         obj_func.SetCoefficient(u[c][d], omega_dict[c][d])
     obj_func.SetMinimization()
 
     # constraint 9 instead of constraint 1
-    for c, d in product(c_list, full_d_list):
+    # note: the due date is a day after the last growth day
+    for c, d in product(C_list, demand_D_list):
         solver.Add(
             SumArray(x[c][gamma_dict[c]][t][d - 1][tau] for t in T_dict[c]) + u[c][d]
             == p_dict[c][d]
         )
+
     # constraint 10 instead of constrain 3
-    for c in c_list:
-        for d in t_list_after(gamma_dict[c] + 1, full_d_list):
+    for c in C_list:
+        for d in t_list_after(gamma_dict[c] + 1, demand_D_list):
             solver.Add(
                 SumArray(x[c][0][sigma][d - gamma_dict[c] - 1][t] for t in T_dict[c])
                 + u[c][d]
@@ -114,29 +137,69 @@ def solve_santini_21_milp_t3(p_ins: ProbInsS21T3, solver_name: str, timelimit: i
             )
 
     # constraint 2: capacity constraints
-    for t1, d, k in product(t_list, d_list, k_list):
-        lhs = SumArray(
-            x[c][g][t1][d][t2]
-            for t2 in t_prime_list
-            for c in c_list
-            if delta_dict[c][t1] and delta_dict[c][t2]
-            for g in t_list_before(gamma_dict[c], full_d_list)
-            if k_dict[c][g] == k
-        )
+    for t1, d, k in product(T_list, growth_D_list, K_list):
+        var_list = list()
+        for t2 in t_prime_list:
+            for c in C_list:
+                if delta_dict[c][t1] and delta_dict[c][t2]:
+                    for g in t_list_before(gamma_dict[c], growth_D_list):
+                        if k_dict[c][g] == k:
+                            var_list.append(x[c][g][t1][d][t2])
+
+        lhs = SumArray(var_list)
         solver.Add(lhs <= q_dict[t1][k] * y[t1][d][k])
 
     # constraint 4: flow-balance constraints
-    for c in c_list:
-        # fix: gamma_dict[c] in paper -> gamma_dict[c] - 1
-        for g in t_list_before(gamma_dict[c] - 1, d_prime_list):
-            for t_1, d in product(t_list, d_list):
+    for c in C_list:
+        # fix
+        for t1, d in product(T_dict[c], growth_D_list):
+            lhs = x[c][0][sigma][d - 1][t1]
+            rhs = SumArray(x[c][1][t1][d][t2] for t2 in T_dict[c])
+            solver.Add(lhs == rhs)
+            for g in t_list_before(gamma_dict[c] - 1, growth_D_list[:-1]):
                 lhs = SumArray(x[c][g][t2][d - 1][t1] for t2 in T_prime_dict[c])
                 rhs = SumArray(x[c][g + 1][t1][d][t2] for t2 in T_prime_dict[c])
                 solver.Add(lhs == rhs)
+            lhs = SumArray(x[c][gamma_dict[c] - 1][t2][d - 1][t1] for t2 in T_dict[c])
+            rhs = x[c][gamma_dict[c]][t1][d][tau]
+            solver.Add(lhs == rhs)
 
     # constraint 5: configuration constraints
-    for t, d in product(t_list, d_list):
-        solver.Add(SumArray(y[t][d][k] for k in k_list) == n_dict[t])
+    for t, d in product(T_list, growth_D_list):
+        solver.Add(SumArray(y[t][d][k] for k in K_list) == n_dict[t])
+
+    # Variable fixing
+    for c, d in product(C_list, D_prime_list):
+        for t, g in product(
+            T_prime_dict[c], t_list_before(gamma_dict[c], D_prime_list)
+        ):
+            solver.Add(x[c][g][t][d][sigma] == 0)
+            solver.Add(x[c][g][tau][d][t] == 0)
+    for c, d in product(C_list, D_prime_list):
+        for g in t_list_before(gamma_dict[c], D_prime_list):
+            solver.Add(x[c][g][sigma][d][tau] == 0)
+
+    for c, d in product(C_list, D_prime_list):
+        for t, g in product(
+            T_prime_dict[c], t_list_before(gamma_dict[c], growth_D_list)
+        ):
+            solver.Add(x[c][g][sigma][d][t] == 0)
+    for c, d in product(C_list, D_prime_list):
+        for t, g in product(
+            T_prime_dict[c], t_list_before(gamma_dict[c] - 1, D_prime_list)
+        ):
+            solver.Add(x[c][g][t][d][tau] == 0)
+    for c, d in product(C_list, growth_D_list):
+        for t1, t2, g in product(
+            T_dict[c], T_dict[c], t_list_before(gamma_dict[c], D_prime_list)
+        ):
+            solver.Add(x[c][g][t1][d][t2] == 0)
+
+    for c, d in product(C_list, D_prime_list):
+        for t1, t2 in product(T_dict[c] + [tau], T_prime_dict[c]):
+            solver.Add(x[c][0][t1][d][t2] == 0)
+        for t1, t2 in product(T_prime_dict[c], [sigma] + T_dict[c]):
+            solver.Add(x[c][gamma_dict[c]][t1][d][t2] == 0)
 
     obj_val: float = 0.0
     solution = VariablesObj3()
@@ -178,20 +241,23 @@ def solve_santini_21_milp_t3(p_ins: ProbInsS21T3, solver_name: str, timelimit: i
                             t2: x[c][g][t1][d][t2].solution_value()
                             for t2 in T_prime_dict[c]
                         }
-                        for d in d_prime_list
+                        for d in D_prime_list
                     }
                     for t1 in T_prime_dict[c]
                 }
-                for g in range(gamma_dict[c] + 1)
+                for g in t_list_before(gamma_dict[c], D_prime_list)
             }
-            for c in c_list
+            for c in C_list
         }
         solution.y = {
-            t: {d: {k: y[t][d][k].solution_value() for k in k_list} for d in d_list}
-            for t in t_list
+            t: {
+                d: {k: y[t][d][k].solution_value() for k in K_list}
+                for d in D_prime_list
+            }
+            for t in T_list
         }
         solution.u = {
-            c: {d: u[c][d].solution_value() for d in full_d_list} for c in c_list
+            c: {d: u[c][d].solution_value() for d in demand_D_list} for c in C_list
         }
 
     return obj_val, solution
